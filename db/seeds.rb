a7c981c5da76351d6ac29dadf20ddc26024748c9
Martino Fabbro <martino.fabbro@gmail.com>
2019-08-09 12:03:29 +0200

    whatever

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+
+Ingredient.create(name: "Beetle Toe")
+Ingredient.create(name: "Blood")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Crickets")
+Ingredient.create(name: "Demon Flesh")
+Ingredient.create(name: "Jew Hashes")
+Ingredient.create(name: "Pig's Feet")
+Ingredient.create(name: "Cocaine")
+Ingredient.create(name: "Monster Zero Ultra")
+Ingredient.create(name: "Rødgrød med fløde")
+Ingredient.create(name: "Fake onions")
